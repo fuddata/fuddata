948a5de41309a1ba5b397fd61dbc7a74d9a625b6
Olli Janatuinen <olljanat@users.noreply.github.com>
2024-01-27 09:29:46 +0000

    Create License: Store initial draft

--- workers/create-license/src/index.js
+++ workers/create-license/src/index.js
@@ -1,15 +1,54 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+async function signData(data, base64PrivKey) {
+  try {
+    // Extract the base64 encoded key body from the PEM format
+    const pemKey = atob(base64PrivKey);
+    const keyBody = pemKey.match(/-----BEGIN EC PRIVATE KEY-----\s*([\s\S]+?)\s*-----END EC PRIVATE KEY-----/)[1];
+    const privKeyBytes = Uint8Array.from(atob(keyBody), c => c.charCodeAt(0));
+
+    const privateKey = await crypto.subtle.importKey(
+      'pkcs8',
+      privKeyBytes.buffer,
+      {
+        name: 'ECDSA',
+        namedCurve: 'P-256'
+      },
+      false,
+      ['sign']
+    );
+    
+    const signature = await crypto.subtle.sign(
+      {
+        name: 'ECDSA',
+        hash: {name: 'SHA-256'},
+      },
+      privateKey,
+      new TextEncoder().encode(data)
+    );
+
+    return arrayBufferToBase64(signature);
+  } catch (err) {
+    console.error("Error:", err);
+    return '';
+  }
+}
+
+function arrayBufferToBase64(buffer) {
+  let binary = '';
+  const bytes = new Uint8Array(buffer);
+  for (let i = 0; i < bytes.byteLength; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return btoa(binary);
+}
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+  async fetch(request, env) {
+    if (request.method != 'POST') {
+      return new Response('', { status: 405 });
+    }
+
+    const dataToSign = await request.text();
+    const signature = await signData(dataToSign, env.PRIVATE_KEY);
+    return new Response(signature, { status: 200 });
+  },
 };
